f43bdae8d83be4e2955f603cca7760eb63a5aabf
nincns <steve.hentschke@me.com>
2022-09-27 21:38:51 +0200

    update

--- bot_start.py
+++ bot_start.py
@@ -1,5 +1,28 @@
-from bot_scanning import MoveServoX, CamScan
+#from bot_scanning import MoveServoX, CamScan
 
-MoveServoX.start()
+import sys
 
-#test
+
+def menu():
+    while True:
+        print(
+            "Selbstcheck",
+            "Geände vermessen"
+            "Umgebung erkunden",
+            "Geräusche erkennen",
+            "Beenden",
+            sep="\n"
+        )
+        choice = int(input("Ihre Wahl? "))
+        if choice == 1:
+            print("Selbstcheck ok")
+        elif choice == 2:
+            print("starte Vermessung")
+        elif choice == 3:
+            print("Starte Erkundung")
+        elif choice == 4:
+            print("höre auf Geräusche")
+        elif choice ==5:
+            sys.exit(0)
+        else:
+            print("Bitte nur Zahlen zwischen 1 und 5 eingeben!")
