c1cca953351d625b5fcc94b1fd20ff3db8671bc0
nincns <steve.hentschke@me.com>
2022-09-28 22:14:12 +0200

    update self check

--- bot_selfcheck.py
+++ bot_selfcheck.py
@@ -1,3 +1,19 @@
-from bot_scanning import MoveServoX, CamScan
+from pitop import LED
 
-MoveServoX.start()
+led_left = LED("D2")
+led_right = LED("D6")
+
+led_left.on()
+if led_left.is_active is True:
+    print("LED_left ok")
+elif led_left.is_active is False:
+    print("LED left not ok")
+led_right.off()
+
+led_right.on()
+if led_right.is_active is True:
+    print("LED right ok")
+elif led_right.is_active is False:
+    print("LED right not ok")
+led_right.off()
+
