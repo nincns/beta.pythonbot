35ac1f760aa649a2b8b032c678fefd2a0d02670e
nincns <steve.hentschke@me.com>
2022-09-28 15:23:42 +0200

    Update bot_start.py

--- bot_start.py
+++ bot_start.py
@@ -19,18 +19,18 @@ def handle_menu(menu):
     while True:
         for index, item in enumerate(menu, 1):
             print("{}  {}".format(index, item[0]))
-        choice = int(input("Ihre Wahl? ")) - 1
+        choice = int(input("start programm? ")) - 1
         if 0 <= choice < len(menu):
             menu[choice][1]()
         else:
-            print("Bitte nur Zahlen im Bereich 1 - {} eingeben".format(
+            print("only numbers between 1 - {} ".format(
                                                                     len(menu)))
 
 menu = [
-    ["start self check", bot_selfcheck],
-    ["discover terrain", discover_terrain],
-    ["start measurement areal dimension", discover_dimension],
-    ["find noise", find_noise],
+    ["01 start self check", bot_selfcheck],
+    ["02 discover terrain", discover_terrain],
+    ["03 start measurement areal dimension", discover_dimension],
+    ["04 find noise", find_noise],
 ]
 
 handle_menu(menu)
