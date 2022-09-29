4e6e024bc35f7b00f303e405cbcb47f46180ded8
nincns <steve.hentschke@me.com>
2022-09-29 13:16:47 +0200

    add check buzzer

--- bot_selfcheck.py
+++ bot_selfcheck.py
@@ -1,10 +1,12 @@
 from pitop import LED
+from pitop import Buzzer
 
 from threading import Thread
 import time
 
 led_left = LED("D0")
 led_right = LED("D6")
+buzzer = Buzzer("D2")
 
 class check_process1(Thread):
     def __init__(self):
@@ -25,5 +27,17 @@ class check_process1(Thread):
             print("LED right not ok")
         led_right.off()
 
-LED_check = check_process1()
+class check_process2(Thread):
+    def __init__(self):
+        Thread.__init__(self)
+    def start(self):
 
+        buzzer.on()
+        if buzzer.is_active is True:
+            print("Buzzer ok")
+        elif buzzer.is_active is False:
+            print("Buzzer not ok")
+        buzzer.off()
+
+LED_check = check_process1()
+Buzzer_check = check_process2()
