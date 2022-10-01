9e4557f1d585673b8e53ce12f3d2288fb111ded0
nincns <steve.hentschke@me.com>
2022-10-01 15:45:22 +0200

    update

--- scanning.py
+++ scanning.py
@@ -43,7 +43,7 @@ class process1(Thread):
         scandirection = "left"
         while self.running: #running process 1
          if servo_pan.current_angle < 90 and scandirection == "left" and self.move is True: #Servo will try start scanning in right direction
-            print("left ",servo_pan.current_angle, "distance ", round(ultrasonic_head.distance.real, 2), "noise ", sound_sensor.reading, "light ", light_sensor.reading)
+            print(servo_pan.current_angle, "distance ", round(ultrasonic_head.distance.real, 2), "noise ", sound_sensor.reading, "light ", light_sensor.reading)
             if servo_pan.current_angle <= 80: #error handling when something interupt process and angle will not 10, 20 or something will execpt with error servo can not set to 90+ degree same for left direction
                 servo_pan.target_angle = servo_pan.current_angle + 10
             elif servo_pan.current_angle >= 81:
@@ -51,7 +51,7 @@ class process1(Thread):
          elif servo_pan.current_angle == 90:
             scandirection = "right"
          if servo_pan.current_angle > -90 and scandirection == "right" and self.move is True:
-            print("right ",servo_pan.current_angle, "distance ", round(ultrasonic_head.distance.real, 2), "noise ", sound_sensor.reading, "light ", light_sensor.reading)
+            print(servo_pan.current_angle, "distance ", round(ultrasonic_head.distance.real, 2), "noise ", sound_sensor.reading, "light ", light_sensor.reading)
             if servo_pan.current_angle >= -80:
                 servo_pan.target_angle = servo_pan.current_angle - 10
             elif servo_pan.current_angle <= -81:
@@ -82,10 +82,10 @@ class process2(Thread):
                 sleep(2)
                 image = cam.get_frame()
                 image.save("pictures/pitop_"+time_now+".jpg")
-                servo_tilt.target_angle = -25
+                servo_tilt.target_angle = 25
                 sleep(2)
                 MoveServoX.resume()
-                MovePiTop.resume()
+                MovePiTop.analyse()
             time.sleep(1)
     def stop(self):
         self.running = False
@@ -123,7 +123,9 @@ class process3(Thread):
         self.turnleft = True
     def right(self):
         self.turnforward = True
-        
+    def analyse(self):
+        print("analyse moving direction")
+
 
 MoveServoX = process1()
 CamScan = process2()
