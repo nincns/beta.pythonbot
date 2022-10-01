971cd2d9992d33cc4091066204e546910a660e3a
nincns <steve.hentschke@me.com>
2022-10-01 22:53:44 +0200

    update

--- discover_terrain.py
+++ discover_terrain.py
@@ -45,6 +45,7 @@ servo_tilt.target_angle = 20
 
 pan_distance = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18,]
 drive_logic = input("please type programm number (1 = discover terrain, 2 = find noise, 3 = find light): ")
+drive_logic = int(drive_logic)
 
 class process1(Thread):
     def __init__(self):
