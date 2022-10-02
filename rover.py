29494e4d1ff7208f36783518df114198c5381706
nincns <steve.hentschke@me.com>
2022-10-02 10:07:26 +0200

    add rover.py

---
+++ rover.py
@@ -0,0 +1,14 @@
+from pitop import Camera, DriveController, PanTiltController, Pitop
+from pitop.labs import RoverWebController
+from pitop.labs.web.blueprints.rover import drive_handler, pan_tilt_handler
+rover = Pitop()
+rover.add_component(DriveController())
+rover.add_component(PanTiltController())
+rover.add_component(Camera())
+rover_controller = RoverWebController(
+    get_frame=rover.camera.get_frame,
+    message_handlers={
+"left_joystick": lambda data: drive_handler(rover.drive, data),
+"right_joystick": lambda data: pan_tilt_handler(rover.pan_tilt, data), },
+)
+rover_controller.serve_forever()
