1652379f62268d498ed5e5b478eee361f31edaad
nincns <steve.hentschke@me.com>
2022-10-02 13:11:22 +0200

    correct comments

--- start.py
+++ start.py
@@ -39,7 +39,7 @@ servo_settings.speed = 50
 #set servo default position
 servo_pan.target_angle = 0
 servo_tilt.target_angle = 20
-#set default array for head ultrasonic maesurements
+#set default array for head ultrasonic maesurements from -90, -80.... 0, 10, 20.... 90 (19valuess)
 pan_distance = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, ]
 #input drive logic
 drive_logic = input("please type programm number (1 = discover terrain, 2 = find noise, 3 = find light): ")
