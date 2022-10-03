dad5a28917a9acb0d02df0a43743b4fa45a36fbb
nincns <steve.hentschke@me.com>
2022-10-03 19:00:28 +0200

    update

--- code_examples/turn.py
+++ code_examples/turn.py
@@ -8,7 +8,8 @@ motor_left.breaking_type = BrakingType.COAST
 motor_right.wheel_diameter=0.05
 motor_left.wheel_diameter=0.05
 
-turnspeed = 0.2
+turnspeed = input("set turnspeed 0.2 - 1.0: ")
+turnspeed = float(turnspeed)
 
 drive_left = input("set drivedistance left: ")
 drive_left = float(drive_left)
@@ -45,9 +46,10 @@ while turn is True:
         motor_right.set_power(turnspeed)
      if rc < motor_right.rotation_counter:
         motor_right.set_power(turnspeed*-1)
-     if rc+0.1 > motor_right.rotation_counter and lc-0.1<motor_right.rotation_counter:
+     if rc+0.1 > motor_right.rotation_counter and rc-0.1<motor_right.rotation_counter:
         motor_right.stop()
-     if motor_left.current_rpm == motor_right.current_rpm:
+     if rc+0.1 > motor_right.rotation_counter and rc-0.1<motor_right.rotation_counter and lc+0.1 > motor_left.rotation_counter and lc-0.1<motor_left.rotation_counter:
         print("arived")
+        turn = False
 
 
